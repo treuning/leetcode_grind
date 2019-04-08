9c9314f0d87a0c6c0eb9a5f287dd1d280f623c1b
Thomas Reuning <thomas.w.reuning@gmail.com>
2019-04-08 15:29:18 -0400

    update

--- 13_roman_to_int_py/romanToInt.py
+++ 13_roman_to_int_py/romanToInt.py
@@ -10,17 +10,26 @@ def romanToInt(s: str) -> int:
         return value
     else:
         for i in range(len(arr)):
+            #add the first numeral to value
             if (i==0):
                 value += d[arr[i]]
+            
+            #if the current numeral is less than the previous, add current to total value
             elif (d[arr[i]] < d[arr[i-1]]):
                 value += d[arr[i]]
+
+            #if the current numeral is greater than the preceding numeral, multiply the previous value
+            # by 2, subtract it from the current value, then add to total value    
             elif (d[arr[i]] > d[arr[i-1]]):
                 value += d[arr[i]] - (d[arr[i-1]]*2) 
+
+            #if the current numeral is equal to the previous, add current to total value
             elif (d[arr[i]] == d[arr[i-1]]):
                 value += d[arr[i]]
-        return abs(value)
+        
+        return value
 
-i1 = "IIII","IX","XIX","XXI","IV","VII","MMMDCCXXIV",'MCMXCIV'
+i1 = "IIII","IX","XIX","XXI","IV","VII","MMMDCCXXIV",'MCMXCIV','IIIIV','IIX'
 
 for i in i1:
     print(romanToInt(i))
